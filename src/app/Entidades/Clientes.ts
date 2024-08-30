44218fb2fbb3e2b9f4fdc01ee10804a34721b852
Corpus691 <120046782+Corpus691@users.noreply.github.com>
2024-08-29 18:26:56 -0600

    Add files via upload

---
+++ src/app/Entidades/Clientes.ts
@@ -0,0 +1,14 @@
+export class Clientes {
+
+    idCliente! : number;
+    //clienteActivo! : String;
+    //clienteNombre! : String;
+    //clienteFechaCreacion! : String;
+    //clienteFechaModificacion! : String;
+    clienteActivo! : number;
+    clienteNombre! : String;
+    clienteFechaCreacion! : String;
+    clienteFechaModificacion! : String;
+    fechacontratacion! : String;
+
+}
